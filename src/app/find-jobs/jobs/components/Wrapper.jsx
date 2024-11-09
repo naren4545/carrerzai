c11a82f1033b0acb42cb96e3324ee758c2cdea16
naren4545 <priyanshusingh.connect@gmail.com>
2024-11-09 12:13:58 +0530

    adding jobs pagr

--- src/app/find-jobs/jobs/components/Wrapper.jsx
+++ src/app/find-jobs/jobs/components/Wrapper.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import JobList from './JobsSection'
 import { JobsProvider } from './UrlContex';
 import HeroSection from './HeroSection'
+import { Suspense } from "react";
 export default function Wrapper() {
   return (
-    <div><JobsProvider>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+      <JobsProvider>
       <HeroSection/>
       <JobList/>
       
       </JobsProvider>
+      </Suspense>
     </div>
   )
 }
