f69af79514dc34eedc77d87e18a048c92c1a4a20
naren4545 <narenchavan26@gmail.com>
2024-11-23 14:37:12 +0530

    changes

--- src/app/find-jobs/jobs/components/JobCard.tsx
+++ src/app/find-jobs/jobs/components/JobCard.tsx
@@ -46,7 +46,7 @@ console.log(job);
           <p className="md:text-2xl text-sm text-gray-500">{job.company}</p>
         </div>
         </div>
-        <WishlistButton id={job._id} Wishlist={job.bookmark}/>
+        <WishlistButton id={job._id} Wishlist={job.bookmark ?? false}/>
       </div>
 
       <hr className="my-2" />
@@ -77,7 +77,7 @@ console.log(job);
           <Link href={`/find-jobs/${job.slug}`}  className="text-blue-600 border  border-blue-600 px-7 py-2 rounded md:mb-2">
             View Details
           </Link>
-         <JobAppy id={job._id} applied={job.applied}/>
+         <JobAppy id={job._id} applied={job.applied?? false}/>
         </div>
       </div>
 
